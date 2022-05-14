f79543afa47b2d39712a068aa2ef09079e70c8d8
Leonardo Hessel <52008953+LeonardoHessel@users.noreply.github.com>
2022-05-14 15:52:39 -0300

    Atividade8

---
+++ Atividade8/Atividade8/MainForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade8
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exercicio1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form fc = Application.OpenForms["frmExercicio1"];
+            if (fc != null)
+                fc.Close();
+
+            frmExercicio1 frm = new frmExercicio1();
+            frm.MdiParent = this;
+            frm.WindowState = FormWindowState.Maximized;
+            frm.Show();
+        }
+
+        private void exercicio2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form fc = Application.OpenForms["frmExercicio2"];
+            if (fc != null)
+                fc.Close();
+
+            frmExercicio2 frm = new frmExercicio2();
+            frm.MdiParent = this;
+            frm.WindowState = FormWindowState.Maximized;
+            frm.Show();
+        }
+
+        private void exercicio3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form fc = Application.OpenForms["frmExercicio3"];
+            if (fc != null)
+                fc.Close();
+
+            frmExercicio3 frm = new frmExercicio3();
+            frm.MdiParent = this;
+            frm.WindowState = FormWindowState.Maximized;
+            frm.Show();
+        }
+
+        private void exercicio4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form fc = Application.OpenForms["frmExercicio4"];
+            if (fc != null)
+                fc.Close();
+
+            frmExercicio4 frm = new frmExercicio4();
+            frm.MdiParent = this;
+            frm.WindowState = FormWindowState.Maximized;
+            frm.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
