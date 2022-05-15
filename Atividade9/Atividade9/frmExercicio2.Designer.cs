92d0181ad48a90e01af1a32c1971d7af34999596
Leonardo Hessel <52008953+LeonardoHessel@users.noreply.github.com>
2022-05-15 12:31:51 -0300

    Atividade 9

---
+++ Atividade9/Atividade9/frmExercicio2.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Atividade9
+{
+    partial class frmExercicio2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtReport = new System.Windows.Forms.RichTextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxtReport
+            // 
+            this.rtxtReport.Location = new System.Drawing.Point(12, 12);
+            this.rtxtReport.Name = "rtxtReport";
+            this.rtxtReport.Size = new System.Drawing.Size(483, 293);
+            this.rtxtReport.TabIndex = 0;
+            this.rtxtReport.Text = "";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(552, 23);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(114, 45);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Ler Produtos";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // frmExercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.rtxtReport);
+            this.Name = "frmExercicio2";
+            this.Text = "frmExercicio2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtReport;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
